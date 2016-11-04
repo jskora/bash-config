f2aa2d4c5743eb86e52b5be0bd996b0bf12e5c8c
Joe Skora <jskora@apache.org>
2016-11-04 09:34:58 -0400

    Updated check.sh.

--- check.sh
+++ check.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#
+# Facilitate verifying that all scripts are included in the setup.sh and diff.sh scripts.
 
 RC=0
 for X in *; do
@@ -7,13 +9,18 @@ for X in *; do
     fi
     grep -E "^safe_copy $X" setup.sh >/dev/null
     if [ $? -ne 0 ]; then
-        echo "not in setup.sh $X"
+        echo "[check.sh] '$X' is not setup in setup.sh"
         RC=1
     fi
     grep -E "^safe_diff $X" diff.sh >/dev/null
     if [ $? -ne 0 ]; then
-        echo "not in diff.sh $X"
+        echo "[check.sh] '$X' is not verified in diff.sh"
         RC=1
     fi
 done
+if [ $RC != 0 ]; then
+    echo "[check.sh] =========================================="
+    echo "[check.sh] Commit rejected until scripts are updated."
+    echo "[check.sh] =========================================="
+fi
 exit $RC
